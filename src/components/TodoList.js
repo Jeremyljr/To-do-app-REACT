67d5c928310ab774a17879b67aa8a48282802e39
jeremyljr <jeremyjiaren@gmil.com>
2021-03-28 19:09:39 +0100

    Added logic

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -7,19 +8,41 @@ function TodoList() {
     const addTodo = todo => {
         //if no letters or space, then wont show
         if(!todo.text || /^\s*$/.test(todo.text)) {
-            return
+            return;
         }
 
         const newTodos = [todo, ...todos]
 
-        setTodos(newTodos)
-        console.log(...todos);
+        setTodos(newTodos);
+    };
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !==id)
+
+        setTodos(removeArr);
     }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id == id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
+
 
     return (
         <div>
             <h1>What's the plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo 
+            todos={todos}
+            completeTodo = {completeTodo}
+            removeTodo={removeTodo}
+            />
         </div>
     )
 }
